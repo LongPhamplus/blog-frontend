3adc7e2de89325b1ba3b1f500485e3b8addcd345
LongPham <longpx1508@gmail.com>
2025-06-12 12:36:45 +0700

    first commit

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { data: posts } = await useAsyncData('blog', () => {
+  return queryCollection('blog').all()
+})
+</script>
+
+<template>
+  <div>
+    <h1>Blog 1</h1>
+    <ul>
+      
+    </ul>
+  </div>
+</template>
---
+++ pages/blog/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { data: blogs } = await useAsyncData(() => {
+    return queryCollection('blog')
+        .all()
+})
+watchEffect(() => {
+    console.log(blogs.value);
+});
+
+</script>
+
+<template>
+    <div>
+        <h1>Blog</h1>
+        <ul>
+            <li v-for="blog in blogs" :key="blog.id">
+                <NuxtLink :to="blog.path">{{ blog.title }}</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
